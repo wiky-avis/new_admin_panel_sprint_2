63f6f1d1b3538ca63f2cacae284e2245101a13b0
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-02-07 10:49:10 +0300

    linters

--- django_api/config/settings.py
+++ django_api/config/settings.py
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "movies",
-"corsheaders",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-"corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -179,4 +179,6 @@ LOGGING = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080",]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080",
+]
--- django_api/config/urls.py
+++ django_api/config/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include('movies.api.urls')),
+    path("api/", include("movies.api.urls")),
 ]
 
 if settings.DEBUG:
